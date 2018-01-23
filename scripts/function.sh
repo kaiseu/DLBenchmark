d0e95d460cbfb621745d82fe8653b2c880a82717
kaiseu <kai.a.xu@intel.com>
2018-01-23 09:07:55 +0800

    add seq data replica

--- scripts/function.sh
+++ scripts/function.sh
@@ -24,3 +24,27 @@ function CHECK_EXIST_EXEC(){
 		chmod +x ${SCRIPT_PATH}
 	fi
 }
+
+function SEQ_DATASET_REPLICA(){
+	ORG_DATASET_DIR=$1
+	DATA_REPLICA=$2
+
+	SUFFIX=".seq"
+
+	ORG_SIZE=`du ${ORG_DATASET_DIR} | awk -F " " '{ print $1 }'`
+	EST_SIZE=$(( ORG_SIZE * DATA_REPLICA / 1024 ))
+	DATE_PREFIX "INFO" "Original dataset is about $((ORG_SIZE/1024)) MB, with replica ${DATA_REPLICA}, it will take about ${EST_SIZE} MB disk space."
+	DATE_PREFIX "INFO" "Creating dataset replica ..."
+
+	
+	for line in `ls -A ${ORG_DATASET_DIR}`
+	do
+		for (( i=1;i<$DATA_REPLICA;i++ ));
+		do
+			NEW_NAME="`echo ${line} | awk 'BEGIN{FS=OFS="."}{$NF=""; NF--; print}'`_${i}${SUFFIX}"
+			cp -r ${ORG_DATASET_DIR}/${line} ${ORG_DATASET_DIR}/${NEW_NAME}
+		done
+	done
+
+	DATE_PREFIX "INFO" "Creating done!"
+}
