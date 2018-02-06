5427d69485ad697353b59ae957f900383cf555cf
kaiseu <kai.a.xu@intel.com>
2018-02-06 11:07:33 +0800

    update caffe ssd weights file name

--- framework/caffe/networks/ssd/bin/predict.sh
+++ framework/caffe/networks/ssd/bin/predict.sh
@@ -49,15 +49,16 @@ fi
 LOCAL_LOG_FILE=logs_${BASE_MODEL}_${IMAGE_RESOLUTION}x${IMAGE_RESOLUTION}_${DATA_SET}_${ITERATION}.log
 
 DATE_PREFIX "INFO" "Predicting Caffe SSD with ${BASE_MODEL} on ${DATA_SET} with image resolution: ${IMAGE_RESOLUTION}x${IMAGE_RESOLUTION} for ${ITERATION} iterations ..."
-sh ${CAFFE_EXEC} test --detection --model ${CAFFE_MODEL_PATH} --weights ${CAFFE_WEIGHTS_PATH} --iterations=${ITERATION} | tee -a ${LOCAL_LOG_FILE}
+echo "${CAFFE_EXEC} test --detection --model ${CAFFE_MODEL_PATH} --weights ${CAFFE_WEIGHTS_PATH} --iterations=${ITERATION} | tee -a ${LOCAL_LOG_FILE}"
+${CAFFE_EXEC} test --detection --model ${CAFFE_MODEL_PATH} --weights ${CAFFE_WEIGHTS_PATH} --iterations=${ITERATION} | tee -a ${LOCAL_LOG_FILE}
 
 if [[ $? == 0 ]]; then
 	DATE_PREFIX "INFO" "Predict Done!"
+	DATE_PREFIX "INFO" "Summary:"
+	echo "Framework    Network    Phase    BaseModel    Dataset    DataReplica    Iterations"
+	echo -e "caffe       SSD       predict  ${BASE_MODEL}    ${DATA_SET}      ${DATA_REPLICA}            ${ITERATION}"
 else
 	 DATE_PREFIX "ERROR" "Predict failed with some error!"
 	 exit -3
 fi
 
-DATE_PREFIX "INFO" "Summary:"
-echo "Framework    Network    Phase    BaseModel    Dataset    DataReplica    Iterations"
-echo -e "caffe	     SSD       predict  ${BASE_MODEL}    ${DATA_SET}      ${DATA_REPLICA}            ${ITERATION}"
